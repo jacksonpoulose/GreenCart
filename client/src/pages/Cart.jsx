ef09c58ac3a3d3ed73e22c25d34c7286e8d20e58
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-11-07 14:42:43 +0000

    cart page

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useAppContext } from "../context/AppContext";
 import { dummyAddress } from "../assets/assets";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
   const {
@@ -30,6 +31,12 @@ const Cart = () => {
     setCartArray(tempArray);
   };
 
+const placeOrder = async () => {
+  if(paymentOption === "COD"){
+    navigate('/order-success');
+  }
+};
+
   useEffect(() => {
     if (products.length > 0 && cartItems) {
       getCart();
@@ -40,7 +47,7 @@ const Cart = () => {
     <div className="flex flex-col md:flex-row mt-16">
       <div className="flex-1 max-w-4xl">
         <h1 className="text-3xl font-medium mb-6">
-          Shopping Cart <span className="text-sm text-indigo-500">{getCartCount()} Items</span>
+          Shopping Cart <span className="text-sm text-primary">{getCartCount()} Items</span>
         </h1>
 
         <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
@@ -55,7 +62,7 @@ const Cart = () => {
             className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 items-center text-sm md:text-base font-medium pt-3"
           >
             <div className="flex items-center md:gap-6 gap-3">
-              <div className="cursor-pointer w-24 h-24 flex items-center justify-center border border-gray-300 rounded overflow-hidden">
+              <div onClick={() => navigate(`/products/${product.category.toLowerCase()}/${product._id}`)} className="cursor-pointer w-24 h-24 flex items-center justify-center border border-gray-300 rounded overflow-hidden">
                 <img
                   className="max-w-full h-full object-cover"
                   src={product.image[0]}
@@ -66,12 +73,13 @@ const Cart = () => {
                 <p className="hidden md:block font-semibold">{product.name}</p>
                 <div className="font-normal text-gray-500/70">
                   <p>
-                    Size: <span>{product.size || "N/A"}</span>
+                    Weight: <span>{product.weight || "N/A"}</span>
                   </p>
                   <div className="flex items-center">
                     <p>Qty:</p>
-                    <select className="outline-none">
-                      {Array(5)
+                    <select onChange={e=>updateCartItem(product._id,Number(e.target.value))}
+                    value={cartItems[product._id]} className="outline-none">
+                      {Array(cartItems[product._id]>9 ? cartItems[product._id] : 9)
                         .fill("")
                         .map((_, index) => (
                           <option key={index} value={index + 1}>
@@ -83,43 +91,15 @@ const Cart = () => {
                 </div>
               </div>
             </div>
-            <p className="text-center">${product.offerPrice * product.quantity}</p>
-            <button className="cursor-pointer mx-auto">
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m12.5 7.5-5 5m0-5 5 5m5.833-2.5a8.333 8.333 0 1 1-16.667 0 8.333 8.333 0 0 1 16.667 0"
-                  stroke="#FF532E"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+            <p className="text-center">{currency}{product.offerPrice * product.quantity}</p>
+            <button onClick={() => removeFromCart(product._id)}  className="cursor-pointer mx-auto">
+             <img src={assets.remove_icon} alt="remove" className="inline-block w-6 h-6" />
             </button>
           </div>
         ))}
 
-        <button className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium">
-          <svg
-            width="15"
-            height="11"
-            viewBox="0 0 15 11"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M14.09 5.5H1M6.143 10 1 5.5 6.143 1"
-              stroke="#615fff"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+        <button onClick={()=>{navigate("/products")}} className="group cursor-pointer flex items-center mt-8 gap-2 text-primary font-medium">
+            <img src={assets.arrow_right_icon_colored} alt="add" className="group-hover:-translate-x-1 transition" />
           Continue Shopping
         </button>
       </div>
@@ -131,24 +111,24 @@ const Cart = () => {
         <div className="mb-6">
           <p className="text-sm font-medium uppercase">Delivery Address</p>
           <div className="relative flex justify-between items-start mt-2">
-            <p className="text-gray-500">No address found</p>
+            <p className="text-gray-500">{selectedAddress ? `${selectedAddress.street},${selectedAddress.city},${selectedAddress.country}` : "No address found"}</p>
             <button
               onClick={() => setShowAddress(!showAddress)}
-              className="text-indigo-500 hover:underline cursor-pointer"
+              className="text-primary hover:underline cursor-pointer"
             >
               Change
             </button>
             {showAddress && (
               <div className="absolute top-12 py-1 bg-white border border-gray-300 text-sm w-full">
-                <p
-                  onClick={() => setShowAddress(false)}
+                {addresses.map((address,index)=>(<p
+                  onClick={() => {setSelectedAddress(address); setShowAddress(false)}}
                   className="text-gray-500 p-2 hover:bg-gray-100"
                 >
-                  New York, USA
-                </p>
+                 {address.street}, {address.city}, {address.country}
+                </p>))}
                 <p
-                  onClick={() => setShowAddress(false)}
-                  className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10"
+                  onClick={() => navigate('/add-address')}
+                  className="text-primary text-center cursor-pointer p-2 hover:bg-primary/10"
                 >
                   Add address
                 </p>
@@ -158,7 +138,7 @@ const Cart = () => {
 
           <p className="text-sm font-medium uppercase mt-6">Payment Method</p>
 
-          <select className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none">
+          <select onChange={e=>setPaymentOption(e.target.value)} className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none">
             <option value="COD">Cash On Delivery</option>
             <option value="Online">Online Payment</option>
           </select>
@@ -169,7 +149,7 @@ const Cart = () => {
         <div className="text-gray-500 mt-4 space-y-2">
           <p className="flex justify-between">
             <span>Price</span>
-            <span>$20</span>
+            <span>{currency}{getCartAmount()}</span>
           </p>
           <p className="flex justify-between">
             <span>Shipping Fee</span>
@@ -177,16 +157,16 @@ const Cart = () => {
           </p>
           <p className="flex justify-between">
             <span>Tax (2%)</span>
-            <span>$20</span>
+            <span>{currency}{(getCartAmount() * 0.02).toFixed(2)}</span>
           </p>
           <p className="flex justify-between text-lg font-medium mt-3">
             <span>Total Amount:</span>
-            <span>$20</span>
+            <span>{currency}{(getCartAmount() + (getCartAmount() * 0.02)).toFixed(2)}</span>
           </p>
         </div>
 
-        <button className="w-full py-3 mt-6 cursor-pointer bg-primary text-white font-medium hover:bg-secondary transition">
-          Place Order
+        <button onClick={placeOrder} className="w-full py-3 mt-6 cursor-pointer bg-primary text-white font-medium hover:bg-secondary transition">
+          {paymentOption === "COD" ? "Place Order" : "Proceed to Payment"}
         </button>
       </div>
     </div>
