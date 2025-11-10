cccfe99704a6171f2421ab888e61939cbc85f68a
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-11-10 14:42:38 +0000

    userside front end finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,19 @@ import AllProducts from "./pages/AllProducts";
 import ProductCategory from "./pages/ProductCategory";
 import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
+import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from "./pages/seller/SellerLayout";
+import ProductList from "./pages/seller/ProductList";
+import Orders from "./pages/seller/Orders";
+import AddProduct from "./pages/seller/AddProduct";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
-  const { showUserLogin } = useAppContext()
+  const { showUserLogin,isSeller } = useAppContext()
   return (
-    <div>
+    <div className="min-h-screen text-default text-gray-700 bg-white">
       <Toaster />
       {isSellerPath ? null : <Navbar />}
       {showUserLogin ? <Login /> : null}
@@ -28,6 +35,14 @@ const App = () => {
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/add-address" element={<AddAddress />} />
+          <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+          <Route index element={isSeller ? <AddProduct/> : null }/>
+          <Route path="product-list" element={isSeller ? <ProductList/> : null }/>
+          <Route path="order" element={isSeller ? <Orders/> : null}/>
+
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
