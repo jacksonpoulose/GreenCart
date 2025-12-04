e7d6c42b6f30f1cd985723ed80868bc82b449882
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-12-04 07:08:44 +0000

    address input field correction

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,89 +1,154 @@
-import React, { useEffect, useState } from 'react'
-import { assets } from '../assets/assets'
-import { useAppContext } from '../context/AppContext'
-import toast from 'react-hot-toast';
+import React, { useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
+
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input
+    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
+    type={type}
+    placeholder={placeholder}
+    name={name}
+    onChange={handleChange}
+    value={address[name]}
+  />
+);
 
 const AddAddress = () => {
-
-const {axios,user, navigate} = useAppContext();
+  const { axios, user, navigate } = useAppContext();
 
   const [address, setAddress] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    street: '',
-    city: '',
-    state: '',
-    zipcode: '',
-  })
-
-
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+  });
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
-    setAddress((prevAddress)=>({...prevAddress, [name]: value}))
-  }
+    setAddress((prevAddress) => ({ ...prevAddress, [name]: value }));
+  };
 
-const InputField = ({type, placeholder, name, handleChange, address})=>(
-  <input className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition' type={type} placeholder={placeholder} name={name} onChange={handleChange} value={address[name]} />
-)
+  // const InputField = ({type, placeholder, name, handleChange, address})=>(
+  //   <input className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition' type={type} placeholder={placeholder} name={name} onChange={handleChange} value={address[name]} />
+  // )
 
-const onSubmitHandler = async (e) => {
-  try{
-    e.preventDefault();
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
 
-    const { data } = await axios.post('/api/address/add', { address });
-    if(data.success){
-      toast.success(data.message);
-      navigate('/cart');
+      const { data } = await axios.post("/api/address/add", { address });
+      if (data.success) {
+        toast.success(data.message);
+        navigate("/cart");
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
-  }catch(error){
-toast.error(error.message);
-  }
- 
-}
+  };
 
-useEffect(() => {
-  if(!user){
-    navigate('/cart');
-  }
-}, [])
+  useEffect(() => {
+    if (!user) {
+      navigate("/cart");
+    }
+  }, []);
 
   return (
     <div className="mt-16 pb-16">
-<p className='text-2xl md:text-3xl text-gray-500'>Add Shipping<span className="font-semibold text-primary"> Address</span></p>
-<div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
-  <div className='flex-1 md:max-w-md'>
-<form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
-  <div className='grid grid-cols-2 gap-2'>
-<InputField type="text" placeholder="First Name" name="firstName" handleChange={handleChange} address={address} />
-<InputField type="text" placeholder="Last Name" name="lastName" handleChange={handleChange} address={address} />
-  </div>
-  <InputField type="email" placeholder="Email" name="email" handleChange={handleChange} address={address} />
-  <InputField type="text" placeholder="Street" name="street" handleChange={handleChange} address={address} />
+      <p className="text-2xl md:text-3xl text-gray-500">
+        Add Shipping<span className="font-semibold text-primary"> Address</span>
+      </p>
+      <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+        <div className="flex-1 md:max-w-md">
+          <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+            <div className="grid grid-cols-2 gap-2">
+              <InputField
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                handleChange={handleChange}
+                address={address}
+              />
+              <InputField
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                handleChange={handleChange}
+                address={address}
+              />
+            </div>
+            <InputField
+              type="email"
+              placeholder="Email"
+              name="email"
+              handleChange={handleChange}
+              address={address}
+            />
+            <InputField
+              type="text"
+              placeholder="Street"
+              name="street"
+              handleChange={handleChange}
+              address={address}
+            />
 
-  <div className='grid grid-cols-2 gap-2'>
-    <InputField type="text" placeholder="City" name="city" handleChange={handleChange} address={address} />
-    <InputField type="text" placeholder="State" name="state" handleChange={handleChange} address={address} />
-    
-  </div>
+            <div className="grid grid-cols-2 gap-2">
+              <InputField
+                type="text"
+                placeholder="City"
+                name="city"
+                handleChange={handleChange}
+                address={address}
+              />
+              <InputField
+                type="text"
+                placeholder="State"
+                name="state"
+                handleChange={handleChange}
+                address={address}
+              />
+            </div>
 
-  <div className='grid grid-cols-2 gap-2'>
-    <InputField type="text" placeholder="Zipcode" name="zipcode" handleChange={handleChange} address={address} />
-    <InputField type="text" placeholder="Country" name="country" handleChange={handleChange} address={address} />
-  </div>
+            <div className="grid grid-cols-2 gap-2">
+              <InputField
+                type="text"
+                placeholder="Zipcode"
+                name="zipcode"
+                handleChange={handleChange}
+                address={address}
+              />
+              <InputField
+                type="text"
+                placeholder="Country"
+                name="country"
+                handleChange={handleChange}
+                address={address}
+              />
+            </div>
 
-<InputField type="text" placeholder="Phone" name="phone" handleChange={handleChange} address={address} />
-<button type="submit" className='w-full bg-primary cursor-pointer text-white px-4 py-2 rounded hover:bg-secondary transition'>Save Address</button>
-
-</form>
-  </div>
-  <img src={assets.add_address_iamge} alt="add address" className='md:mr-16 mb-16 md:mt-0' />
-
-</div>
+            <InputField
+              type="text"
+              placeholder="Phone"
+              name="phone"
+              handleChange={handleChange}
+              address={address}
+            />
+            <button
+              type="submit"
+              className="w-full bg-primary cursor-pointer text-white px-4 py-2 rounded hover:bg-secondary transition"
+            >
+              Save Address
+            </button>
+          </form>
+        </div>
+        <img src={assets.add_address_iamge} alt="add address" className="md:mr-16 mb-16 md:mt-0" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddAddress
+export default AddAddress;
